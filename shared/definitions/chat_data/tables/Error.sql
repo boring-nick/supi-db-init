c4575264c3ba0b523cdf243b794f4841a4aa584c
Supinic <supinic@protonmail.com>
2022-10-04 19:25:15 +0200

    added missing `Request` error type
    - resolves #2

--- shared/definitions/chat_data/tables/Error.sql
+++ shared/definitions/chat_data/tables/Error.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `chat_data`.`Error` (
   `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `Type` enum('Backend','Command','Database','Website','Website - API','Other') NOT NULL,
+  `Type` enum('Backend','Command','Database','Website','Website - API','Other','Request') NOT NULL,
   `Origin` enum('Internal','External') DEFAULT NULL,
   `Message` mediumtext DEFAULT NULL,
   `Stack` text NOT NULL DEFAULT '',
